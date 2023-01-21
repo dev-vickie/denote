e5d3bd62ff2a4b6f6f43b4273e41b1ec9c92f6c4
dev-vickie <vmutethia84@gmail.com>
2023-01-21 07:00:13 +0300

    FR - change minSdkVersion to 21

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:denote/auth/pages/login_page.dart';
+import 'package:denote/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
